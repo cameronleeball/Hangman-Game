81ca55cf1fa5d793ddedb3e12009901e1feda639
Cameron Lee Ball <cameronleeball@gmail.com>
2017-03-09 18:29:01 -0500

    hangman submission

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,41 +8,88 @@ var nCharName = ["the dude", "walter", "donnie",  "brant", "bunny", "maude"];
 var hCharName = ["quintana", "jackie treehorn", "jeffery lebowski", "karl hungus", "knox harrington"];
 var curName;
 var curLetters;
-var difficulty;
 var userGuess = "";
-var score;
+var score = {
+	misses: 0, /* updates for */
+	remGuess: 6, /* head, torso, l arm, r arm, l leg, r leg */
+	remLet: 0, /* curName.length */
+	sucGuess: 0, 
+	sucWords: 0
+};
+var prevLets = [];
 var run;
+var dif = [];
 
-function help() {
-	alert("Guess the letters to fill in the blanks. After x number of wrong choices, you loose.");
-}
-
-function setup() {
-	userGuess = "";
-	score = {
-		misses:0, /* updates for */
-		remGuess:6, /* head, torso, l arm, r arm, l leg, r leg */
-		remLet: curName.length, /* curName.length */
-		sucGuess:0, 
-		sucWords:0
-	};
+function updateScore() {
 	document.getElementById("userGuess").innerHTML = userGuess;
 	document.getElementById("misses").innerHTML = score.misses;
 	document.getElementById("remLet").innerHTML = score.remLet;
 	document.getElementById("remGuess").innerHTML = score.remGuess;
 	document.getElementById("sucGuess").innerHTML = score.sucGuess;
-	return;
+	document.getElementById("sucWords").innerHTML = score.sucWords;
+	document.getElementById("prevLets").innterHTML = prevLets;
 }
 
-// scoreboard obj
-
-function play() {
-
+function help() {
+	alert("Guess the letters to fill in the blanks. After x number of wrong choices, you loose.");
 }
 
-function normal() {
-	curName = nCharName[Math.floor(Math.random() * nCharName.length)];
+function reset() {
+	userGuess = "";
+	score.misses= 0; /* updates for */
+	score.remGuess = 6; /* head, torso, l arm, r arm, l leg, r leg */
+	score.remLet= 0; /* curName.length */
+	score.sucGuess = 0; 
+	prevLets = [];
+	updateScore();
+}
+
+
+function winCond() {
+	if (score.remGuess <= 0) { 
+		updateScore();
+		alert("OVER THE LINE! Mark it Zero... next frame.");
+	}
+	else if ((score.remGuess >= 0) && (score.remLet == 0)) {
+		updateScore();
+		alert("Far Out");
+		score.sucWords++;
+		reset();
+	}
+}
+
+function setup() {
+	userGuess = "";
+	updateScore();
+}
+
+function guessTrue() {
+	score.remLet--;
+	score.sucGuess++;
+	updateScore();
+}
+
+function guessFalse() {
+	score.misses++;
+	score.remGuess--;
+	updateScore();
+}
+
+function setDifN() {
+	dif = nCharName;
+	return dif;
+}
+
+function setDifH() {
+	dif = hCharName;
+	return dif;
+	$("#startBtn").attr()
+}
+
+function setLevel() {
+	curName = dif[Math.floor(Math.random() * dif.length)];
 	curLetters = curName.split("");
+	score.remLet = curName.length;
 	setup();
 	console.log(curLetters);
 	console.log(score);
@@ -52,62 +99,39 @@ function normal() {
 	}
 	console.log(blanks);
 	document.getElementById("letterbox").innerHTML = blanks;
+}
+
+function playLevel() {
 	document.onkeyup = function(event) {
 		userGuess = event.key;
 		console.log(userGuess);
-		console.log(score.misses);
+		console.log(score);
 		curLetters.forEach(function(curLetter, i) { 
 			if (curLetter == userGuess)
 				blanks[i] = userGuess; 
 			document.getElementById("letterbox").innerHTML = blanks;});
 
 		if ((curLetters.includes(userGuess)) === false) {
-			score.misses = (score.misses + 1);
-			score.remGuess = (score.remGuess - 1);
-			document.getElementById("remGuess").innerHTML = score.remGuess;
-			document.getElementById("misses").innerHTML = score.misses;
-			console.log(score.misses);
+			guessFalse();
 		}
 
 		else if ((curLetters.includes(userGuess)) === true) { 
-			score.remLet = (score.remLet - 1);
-			score.sucGuess = (score.sucGuess + 1);
-			document.getElementById("remLet").innerHTML = score.remLet;
-			document.getElementById("sucGuess").innerHTML = score.sucGuess;
+			guessTrue();
 		}
-
 		document.getElementById("userGuess").innerHTML = userGuess;	
-		if (score.remGuess <= 0) { 
-			alert("OVER THE LINE! Mark it Zero... next frame.");
-
-		}
+		winCond();
 	}
-	
 }
 
+function play() {
+	if (dif == 'undefined') {
+		alert("Please select a difficulty!");
+		
+	}
+	else {
+		setLevel();
+		playLevel();
+	}
+}
 
-	// while (remGuess >=0) {
-	// 	return true;
-	// 	if (false) {
-	// 		break;
-	// 	}
-	// }
-
-
-
-
-// function hard() {
-// 	curName = hCharName[Math.floor(Math.random() * hCharName.length)];
-// 	curLetters = curName.split("");
-// 	// console.log(curLetters);
-// 	blanks = [];
-// 	for (var i = 0; i < curLetters.length; i++) {
-// 		blanks.push("_ ");
-// 	}
-// 	console.log("blanks");
-// }
-
-// function main(){
-// 	normal();
-// }
 
